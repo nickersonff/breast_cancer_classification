6250b5a3bb2aee73c8245bd960e1cc650bfe8d7b
Nickerson <nickersonfonseca@hotmail.com>
2026-09-03 09:55:47 -0300

    add new models and fix requirements.txt

---
+++ src/pt/models/fccs-net.py
@@ -0,0 +1,88 @@
+num_classes=2
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+# Define the CBAM module
+class CBAM(nn.Module):
+    def __init__(self, in_channels):
+        super(CBAM, self).__init__()
+        self.channel_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, in_channels // 16, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels // 16, in_channels, kernel_size=1),
+            nn.Sigmoid()
+        )
+        self.spatial_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            #nn.MaxPool2d(1),
+            nn.Conv2d(in_channels, 1, kernel_size=7, padding=3),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        x_channel_att = self.channel_att(x)
+        x_spatial_att = self.spatial_att(x)
+        x_att = torch.mul(x_channel_att, x_spatial_att)
+        return torch.mul(x, x_att)
+
+#CBAM with residual connection
+class ResNet18_CBAM(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNet18_CBAM, self).__init__()
+        self.resnet = models.resnet18(pretrained=True)
+
+        # Disable gradients for all the parameters in the pre-trained ResNet18
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        # Enable gradients for the last two layers of ResNet18
+        for param in self.resnet.layer4.parameters():
+            param.requires_grad = False
+        for param in self.resnet.layer3.parameters():
+            param.requires_grad = True
+            
+        self.cbam1 = CBAM(64)  # Apply CBAM to the output of layer3
+        self.cbam2 = CBAM(128)  # Apply CBAM to the output of layer4
+        self.cbam3 = CBAM(256)  # Apply CBAM to the output of layer3
+        self.cbam4 = CBAM(512)  # Apply CBAM to the output of layer4
+
+        self.global_avg_pooling = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc1 = nn.Linear(960, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x_res1 = self.resnet.layer1(x)
+        x_cbam1 = self.cbam1(x_res1)
+        x1 = x_res1 + x_cbam1
+
+        x_res2 = self.resnet.layer2(x1)
+        x_cbam2 = self.cbam2(x_res2)
+        x2 = x_res2 + x_cbam2
+
+        x_res3 = self.resnet.layer3(x2)
+        x_cbam3 = self.cbam3(x_res3)
+        x3 = x_res3 + x_cbam3
+
+        x_res4 = self.resnet.layer4(x3)
+        x_cbam4 = self.cbam4(x_res4)
+        x4 = x_res4 + x_cbam4
+
+        x_cbam1_gap = self.global_avg_pooling(x_cbam1)
+        x_cbam2_gap = self.global_avg_pooling(x_cbam2)
+        x_cbam3_gap = self.global_avg_pooling(x_cbam3)
+        x_cbam4_gap = self.global_avg_pooling(x_cbam4)
+
+        x_cbam_concat = torch.cat([x_cbam1_gap, x_cbam2_gap, x_cbam3_gap, x_cbam4_gap], dim=1)
+        x_cbam_concat = x_cbam_concat.view(x_cbam_concat.size(0), -1)
+        out = self.fc1(x_cbam_concat)
+        out = self.fc2(out)
+        return out
+
+
---
+++ src/pt/models/mammo_ensemble.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.nn.functional as F
+from torchvision.models import VGG16_BN_Weights
+
+class MammographyEnsemble(nn.Module):
+    def __init__(self, num_classes=2):
+        """
+        Comitê de CNNs para detecção de câncer de mama.
+        num_classes=2 geralmente representa (0: Benigno/Normal, 1: Maligno)
+        """
+        super(MammographyEnsemble, self).__init__()
+        
+        # 1. Carregando e adaptando a ResNet-50
+        self.model1 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        # Substitui a última camada (head) para o nosso número de classes
+        num_features = self.model1.fc.in_features
+        self.model1.fc = nn.Sequential(
+            nn.Linear(num_features, 256),  # Additional linear layer with 256 output features
+            nn.ReLU(inplace=True),         # Activation function (you can choose other activation functions too)
+            nn.Dropout(0.5),               # Dropout layer with 50% probability
+            nn.Linear(256, num_classes)              # Final prediction fc layer
+        )
+        
+        # 2. Carregando e adaptando a DenseNet-121
+        self.model2 = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
+        num_features = self.model2.classifier.in_features
+        self.model2.classifier = nn.Sequential(
+                nn.Linear(num_features, 256),  # Additional linear layer with 256 output features
+                nn.ReLU(inplace=True),         # Activation function (you can choose other activation functions too)
+                nn.Dropout(0.5),               # Dropout layer with 50% probability
+                nn.Linear(256, num_classes)              # Final prediction fc layer
+        )
+
+        self.model3 = models.vgg16_bn(weights=VGG16_BN_Weights.IMAGENET1K_V1)
+        num_features = self.model3.classifier[6].in_features
+        nova_camada_final = nn.Sequential(
+            nn.Linear(num_features, 256),  # Additional linear layer with 256 output features
+            nn.ReLU(inplace=True),         # Activation function (you can choose other activation functions too)
+            nn.Dropout(0.5),               # Dropout layer with 50% probability
+            nn.Linear(256, num_classes)              # Final prediction fc layer
+        )
+        self.model3.classifier[6] = nova_camada_final
+
+    def init_weights(self,w_model1, w_model2, w_model3):
+        model_data1 = torch.load(w_model1)
+        #print(model_data)
+        self.model1.load_state_dict(model_data1['model_weights'])
+
+        model_data2 = torch.load(w_model2)
+        #print(model_data)
+        self.model2.load_state_dict(model_data2['model_weights'])
+
+        model_data3 = torch.load(w_model3)
+        #print(model_data)
+        self.model3.load_state_dict(model_data3['model_weights'])
+
+    def forward(self, x):
+        # Extração dos "logits" (saídas cruas) de cada modelo
+        out1 = self.model1(x)
+        out2 = self.model2(x)
+        out3 = self.model3(x)
+        
+        # Aplicação da função Softmax para transformar os logits em probabilidades (0 a 1)
+        prob1 = F.softmax(out1, dim=1)
+        prob2 = F.softmax(out2, dim=1)
+        prob3 = F.softmax(out3, dim=1)
+        
+        # Soft Voting: Média das probabilidades estimadas pelos 3 modelos
+        avg_prob = (prob1 + prob2 + prob3) / 3.0
+        
+        return avg_prob
